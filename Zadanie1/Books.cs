fc666f26c02defdb53adf1b1de897cf855dbce96
dzaczek <sirjacek@gmail.com>
2015-04-13 22:03:45 +0200

    Out

---
+++ Zadanie1/Books.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Books
+    {
+        private string title;
+        private  int isbn;
+        private string author;
+
+        public Books(string title,int isbn,string author)
+        {
+            this.author = author;
+            this.isbn = isbn;
+            this.title = title;
+
+
+        }
+
+        public void PrintBooks() { Console.Out.WriteLine("Title: {0} \n Author: {1} \n,ISBN : {2}", title, author, isbn); }
+
+
+    }
+}
---
+++ Zadanie1/Readers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Readers
+    {
+        private int age;
+        private string name;
+
+
+        public Readers()
+        {
+            name = "N/A";
+        }
+
+        public Readers(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+
+        }
+
+        public void PrintReaders()
+        {
+            Console.WriteLine("User {0}, age {1}", name, age);
+        }
+
+
+    }
+
+    
+
+
+
+}
